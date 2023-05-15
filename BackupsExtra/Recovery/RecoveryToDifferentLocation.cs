7fd0c82a5ea80568b159b68d195249818c6f7e00
6utcher <89826699+vvvkks@users.noreply.github.com>
2023-05-15 12:47:30 +0300

    Add files via upload

---
+++ BackupsExtra/Recovery/RecoveryToDifferentLocation.cs
@@ -0,0 +1,38 @@
+﻿using Backups.Entities;
+using Backups.Extra.Entities;
+using Backups.Extra.Logging;
+
+namespace Backups.Extra.Recovery;
+
+public class RecoveryToDifferentLocation : IRecovery
+{
+    private readonly ILogging _logging = new ConsoleLogging();
+    private readonly ExtraRestorePoint _extraRestorePoint;
+
+    public RecoveryToDifferentLocation(ExtraRestorePoint extraRestorePoint)
+    {
+        _extraRestorePoint = extraRestorePoint;
+    }
+
+    public void Recovery()
+    {
+        var pathToRecovery = new List<string>();
+        if (!Directory.Exists(pathToRecovery.ToString()))
+        {
+            Directory.CreateDirectory(pathToRecovery.ToString() !);
+        }
+
+        foreach (ExtraRepository repository in _extraRestorePoint.GetRepositories())
+        {
+            foreach (Storage file in repository.Storages)
+            {
+                _logging.Write("Adding path to recovery");
+                pathToRecovery.Add(file.Path);
+            }
+        }
+
+        var specifiedDirectory = new DirectoryInfo(pathToRecovery.ToString() !);
+        specifiedDirectory.Create();
+        _logging.Write("Specified Directory was created");
+    }
+}
---
+++ BackupsExtra/Recovery/RecoveryToOriginalLocation.cs
@@ -0,0 +1,29 @@
+﻿using Backups.Entities;
+using Backups.Extra.Entities;
+using Backups.Extra.Logging;
+
+namespace Backups.Extra.Recovery;
+
+public class RecoveryToOriginalLocation : IRecovery
+{
+    private readonly ILogging _logging = new ConsoleLogging();
+    private readonly ExtraRestorePoint _extraRestorePoint;
+    public RecoveryToOriginalLocation(ExtraRestorePoint extraRestorePoint)
+    {
+        _extraRestorePoint = extraRestorePoint;
+    }
+
+    public void Recovery()
+    {
+        var pathToRecovery = new List<string>();
+        foreach (ExtraRepository repository in _extraRestorePoint.GetRepositories())
+        {
+            foreach (Storage file in repository.Storages)
+            {
+                _logging.Write("Adding path to recovery");
+                pathToRecovery.Add(file.Path);
+                _logging.Write("Recovery completed");
+            }
+        }
+    }
+}
