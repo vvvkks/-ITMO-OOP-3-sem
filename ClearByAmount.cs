88f7b33a5da77f4c0542888601231b4ed75ada8e
6utcher <89826699+vvvkks@users.noreply.github.com>
2023-05-15 12:42:25 +0300

    Add files via upload

---
+++ ClearByAmount.cs
@@ -0,0 +1,28 @@
+﻿using Backups.Entities;
+using Backups.Extra.Exception;
+using Backups.Extra.Logging;
+
+namespace Backups.Extra.ClearAlgorithms;
+
+public class ClearByAmount : ISearchingAlgorithm
+{
+    private readonly int _limitAmount;
+
+    public ClearByAmount(int amount)
+    {
+        if (amount <= 0)
+        {
+            throw new BackupsExtraException("Amount can't be zero or less");
+        }
+
+        _limitAmount = amount;
+    }
+
+    public List<RestorePoint> GetPoints(List<RestorePoint> restorePoints)
+    {
+        var restorePointsToSave = new List<RestorePoint>();
+        int count = restorePoints.Count - _limitAmount;
+        var points = restorePointsToSave.Take(count).ToList();
+        return points;
+    }
+}
