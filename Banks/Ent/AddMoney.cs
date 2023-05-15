74d6cad5ef5c872f870c7e726e8eaa3d9fb71327
6utcher <89826699+vvvkks@users.noreply.github.com>
2023-05-15 12:36:17 +0300

    Add files via upload

---
+++ Banks/Ent/AddMoney.cs
@@ -0,0 +1,27 @@
+﻿using Banks.Entities.Accounts;
+
+namespace Banks.Entities.Transactions;
+
+public class AddMoney : Transaction
+{
+    private readonly Account _account;
+    private readonly decimal _money;
+
+    public AddMoney(Account account, decimal money)
+    {
+        _account = account;
+        _money = money;
+    }
+
+    public override void Cancel()
+    {
+        IsTransactionExecuted = true;
+        _account.DeductFunds(_money);
+    }
+
+    public override void Execute()
+    {
+        IsTransactionExecuted = false;
+        _account.CreditFunds(_money);
+    }
+}
