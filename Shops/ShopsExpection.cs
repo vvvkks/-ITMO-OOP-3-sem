d74b6e3777da8fcea2fe6e220c38136fa70b9ffa
6utcher <89826699+vvvkks@users.noreply.github.com>
2023-05-15 11:11:05 +0300

    Add files via upload

---
+++ Shops/ShopsExpection.cs
@@ -0,0 +1,15 @@
+﻿namespace Shops.Expection;
+
+public class ShopsExpection : Exception
+{
+    public ShopsExpection(string message)
+        : base(message)
+    {
+    }
+
+    public ShopsExpection(string message, Exception innerException)
+        : base(message, innerException)
+    {
+        throw new NotImplementedException();
+    }
+}
