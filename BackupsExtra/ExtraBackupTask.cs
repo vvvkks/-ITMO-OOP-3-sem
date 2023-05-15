3c53bbf382bcc4588d47c1d88d74e213c1c43607
6utcher <89826699+vvvkks@users.noreply.github.com>
2023-05-15 12:44:42 +0300

    Add files via upload

---
+++ BackupsExtra/ExtraBackupTask.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+using Backups.Entities;
+using Backups.Extra.ClearAlgorithms;
+using Backups.Extra.Logging;
+using Backups.Services;
+using Newtonsoft.Json;
+
+namespace Backups.Extra.Entities;
+
+public class ExtraBackupTask : BackupTask
+{
+    private readonly List<RestorePoint> _restorePoints;
+    private readonly ILogging _logging = new ConsoleLogging();
+    public ExtraBackupTask(IAlgorithm algorithm, IRepository repository, string name, ILogging logging, ISearchingAlgorithm searchingAlgorithm)
+        : base(algorithm, repository, name)
+    {
+        SearchingAlgorithm = searchingAlgorithm;
+        Logging = logging;
+        _restorePoints = new List<RestorePoint>();
+    }
+
+    public ISearchingAlgorithm SearchingAlgorithm { get; }
+    public ILogging Logging { get; }
+
+    public ReadOnlyCollection<RestorePoint> GetRestorePoints()
+    {
+        _logging.Write("Getting All RestorePoints");
+        return _restorePoints.AsReadOnly();
+    }
+}
