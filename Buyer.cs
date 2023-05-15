422d6a3c2c8204a8ebbf68ff1ac1137164473e98
6utcher <89826699+vvvkks@users.noreply.github.com>
2023-05-15 11:07:16 +0300

    Add files via upload

---
+++ Buyer.cs
@@ -0,0 +1,40 @@
+﻿using Shops.Expection;
+
+namespace Shops.Entities
+{
+    public class Buyer
+    {
+        public const int MinimalMoney = 0;
+        private List<Product> _products;
+
+        public Buyer(string name, decimal money, int id)
+        {
+            if (name == null)
+                throw new ArgumentNullException("Wrong buyer name");
+            if (money < MinimalMoney)
+                throw new ShopsExpection("Not enough money");
+            Name = name;
+            Money = money;
+            _products = new List<Product>();
+            Id = id;
+        }
+
+        public int Id { get; }
+        public string Name { get; }
+        public decimal Money { get; private set; }
+        public IReadOnlyList<Product> Products => _products;
+        public Buyer SpendMoney(decimal productPrice, Buyer client)
+        {
+            if (Money >= productPrice)
+            {
+                Money -= productPrice;
+            }
+            else
+            {
+                throw new ShopsExpection("Not enough money");
+            }
+
+            return client;
+        }
+    }
+}
