25de89dbe5281e37d90639d64509d7a36b2e161a
6utcher <89826699+vvvkks@users.noreply.github.com>
2023-05-15 12:38:09 +0300

    Add files via upload

---
+++ Banks/BanksException.cs
@@ -0,0 +1,17 @@
+﻿namespace Banks.Exception;
+public class BanksException : System.Exception
+{
+    public BanksException()
+    {
+    }
+
+    public BanksException(string message)
+            : base(message)
+        {
+        }
+
+    public BanksException(string message, System.Exception innerException)
+            : base(message, innerException)
+        {
+        }
+}
