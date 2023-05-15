cc7b50525488219af8b6a4c94c25b031c6fea167
6utcher <89826699+vvvkks@users.noreply.github.com>
2023-05-15 11:09:36 +0300

    Add files via upload

---
+++ Shops/Services/IMarketplaceService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Shops.Entities;
+
+namespace Shops.Services
+{
+    public interface IMarketplaceService
+    {
+        Buyer AddBuyer(string name, decimal money);
+        Shop AddShop(string name, string address);
+        Product GetProduct(Shop shop, string name);
+        void AddProduct(List<Product> products, Shop shop);
+        void ChangePriceProduct(Shop shop, string name, decimal newPrice);
+        Shop FindCheapestProduct(List<Product> products);
+        void BuyProduct(Buyer buyer, Shop shop, List<ProductTuple> shopList);
+    }
+}
