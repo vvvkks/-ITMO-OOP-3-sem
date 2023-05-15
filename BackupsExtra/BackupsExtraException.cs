f6c49436ca3e6200988fd394b0cb5ea8f2f767de
6utcher <89826699+vvvkks@users.noreply.github.com>
2023-05-15 12:47:58 +0300

    Add files via upload

---
+++ BackupsExtra/BackupsExtraException.cs
@@ -0,0 +1,14 @@
+﻿namespace Backups.Extra.Exception;
+
+public class BackupsExtraException : System.Exception
+{
+    public BackupsExtraException(string message)
+            : base(message)
+        {
+        }
+
+    public BackupsExtraException(string message, System.Exception innerException)
+            : base(message, innerException)
+        {
+        }
+}
