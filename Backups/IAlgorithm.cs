14d08b94b0a5ae36cc8f0fa50999d6daed12bc66
6utcher <89826699+vvvkks@users.noreply.github.com>
2023-05-15 11:21:20 +0300

    Add files via upload

---
+++ Backups/IAlgorithm.cs
@@ -0,0 +1,7 @@
+﻿using Backups.Entities;
+namespace Backups.Services;
+
+public interface IAlgorithm
+{
+    IEnumerable<Storage> CreateStorages(IReadOnlyCollection<BackupObject> backupObjects, int number, IRepository repository);
+}
