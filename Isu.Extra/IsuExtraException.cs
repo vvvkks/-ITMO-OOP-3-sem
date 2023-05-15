a9a4dcd1971d77456fef2f4342400ed111e2dec4
6utcher <89826699+vvvkks@users.noreply.github.com>
2023-05-15 11:16:14 +0300

    Add files via upload

---
+++ Isu.Extra/IsuExtraException.cs
@@ -0,0 +1,15 @@
+﻿namespace Isu.Extra.Exception;
+
+public class IsuExtraException : System.Exception
+{
+    public IsuExtraException(string message)
+        : base(message)
+    {
+    }
+
+    public IsuExtraException(string message, System.Exception innerException)
+        : base(message, innerException)
+    {
+        throw new NotImplementedException();
+    }
+}
