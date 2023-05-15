7abaa9b985b547f497099c118cd4351ff92d9a0f
6utcher <89826699+vvvkks@users.noreply.github.com>
2023-05-15 11:21:55 +0300

    Add files via upload

---
+++ Backups/Algorithms/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using Backups.Entities;
+
+namespace Backups.Services;
+
+public interface IRepository
+{
+    string FullPath { get; }
+    byte[] ReadAllBytes(string path);
+    void WriteAllText(string path, string cont);
+    public IReadOnlyCollection<Storage> GetStorages();
+    void Save(BackupTask backupTask);
+}
