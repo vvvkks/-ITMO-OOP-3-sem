ac6d10eeec990ff4c67b81127385821ca10164d5
6utcher <89826699+vvvkks@users.noreply.github.com>
2023-05-15 11:19:53 +0300

    Add files via upload

---
+++ BackupObject.cs
@@ -0,0 +1,21 @@
+﻿using Backups.Exception;
+namespace Backups.Entities;
+public class BackupObject
+{
+    public BackupObject(string path = "", string fileName = "")
+    {
+        var fileInfo = new FileInfo(System.IO.Path.Combine(path, fileName));
+        Name = fileInfo.Name;
+        Path = path;
+        FullName = System.IO.Path.Combine(path, fileName);
+    }
+
+    public string Name { get; }
+    public string Path { get; }
+
+    public string FullName { get; }
+    public bool Exist()
+    {
+        return File.Exists(Path);
+    }
+}
