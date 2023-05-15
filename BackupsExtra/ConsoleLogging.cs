396955f2b5bb5bf77b6577ef210e19b17db621c4
6utcher <89826699+vvvkks@users.noreply.github.com>
2023-05-15 12:45:50 +0300

    Add files via upload

---
+++ BackupsExtra/ConsoleLogging.cs
@@ -0,0 +1,9 @@
+﻿namespace Backups.Extra.Logging;
+
+public class ConsoleLogging : ILogging
+{
+    public void Write(string message, bool isTimecodeOn = false)
+    {
+        Console.WriteLine($"{DateTime.Now} | message + ':' ");
+    }
+}
