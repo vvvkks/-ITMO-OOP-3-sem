a0dd3652237859ccd679d005acb614caff620ff1
6utcher <89826699+vvvkks@users.noreply.github.com>
2023-05-15 11:14:49 +0300

    Add files via upload

---
+++ Isu.Extra/IIsuExtraService.cs
@@ -0,0 +1,18 @@
+﻿using Isu.Extra.Enities;
+using Isu.Extra.Exception;
+
+namespace Isu.Extra.Services;
+public interface IIsuExtraService
+{
+    Ognp AddOgnp(string name, char faculty);
+    IsuExtraGroup NewExtraGroup(string name, Schedule schedule);
+    IsuExtraStudent NewExtraStudent(string name, IsuExtraGroup group);
+    GroupOgnp AddGroupOgnp(string name, CurrentOgnp ognp, string audience, string lector, Schedule schedule);
+    void AddOgnpForStudent(IsuExtraStudent student, CurrentOgnp studentOgnp, GroupOgnp groupOgnp);
+    CurrentOgnp AddCurrentOgnp(string name, Ognp ognp, Schedule schedule, string lector, string audience, char faculty);
+    IReadOnlyList<CurrentOgnp> GetCurrentOgnps(Ognp ognp);
+    IsuExtraStudent RemoveFromOgnpGroup(IsuExtraStudent student, GroupOgnp groupOgnp);
+    IsuExtraStudent RemoveFromCurrentOgnp(IsuExtraStudent student, CurrentOgnp currentOgnp);
+    IReadOnlyList<IsuExtraStudent> GetStudents(GroupOgnp groupOgnp);
+    IReadOnlyList<IsuExtraStudent> GetStudentsWithoutOgnp(GroupOgnp groupOgnp);
+}
